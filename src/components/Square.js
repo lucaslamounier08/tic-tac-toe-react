97a5a56028a76b285b78fb46acc1392c99066bb7
lucaslamounier08 <lucaslamounier08@gmail.com>
2024-10-21 20:00:12 -0300

    chore: onSquareClick }) {

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,3 @@
-import { useState } from 'react';
-
-export default function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue(value === 'X' ? 'O' : 'X');
-        console.log('handleClick: ' + value);
-    }
-
-    return <button className="square" onClick={handleClick}>{value}</button>;
+export default function Square({ value, onSquareClick }) {
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
